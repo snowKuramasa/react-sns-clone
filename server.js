b6d3b802f1c9f99c1466f98d8213793a44059689
kuramasa <kuramasa.green@gmail.com>
2023-08-17 22:20:09 +0900

    ルーティングサンプル設定

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("node.js")
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+//ルーター呼び出し
+const usersRouter = require('./routes/users')
+
+//ミドルウェア
+//ベースURI
+app.use('/api/users', usersRouter)
+
+//エンドポイント
+app.get('/', (req, res) => {
+  res.send('hello express')
+})
+
+// app.get('/users', (req, res) => {
+//   res.send('hello users')
+// })
+
+app.listen(PORT, () => console.log('サーバー起動'))
