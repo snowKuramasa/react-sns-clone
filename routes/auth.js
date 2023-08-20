1c8d5e78f347b73f4f4a7037604aa6c4f5697fa1
kuramasa <kuramasa.green@gmail.com>
2023-08-20 16:59:00 +0900

    記述ミス修正、認証用のユーザー登録API作成

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,25 @@
 const router = require('express').Router()
+const User = require('./models/User')
+
+//ユーザ登録
+router.post('/register', async (req, res) => {
+  try {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    })
+    const result = await newUser.save()
+
+    return res.status(201).json(result)
+  } catch (err) {
+    return res.status(500).json(err)
+  }
+})
 
 //server.jsのURIをルートとしている
-router.get('/', (req, res) => {
-  res.send('auth router')
-})
+// router.get('/', (req, res) => {
+//   res.send('auth router')
+// })
 
 module.exports = router
