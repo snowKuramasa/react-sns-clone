da11ff9d46bf1ecea965a5cf48e367e77354cf17
kuramasa <kuramasa.green@gmail.com>
2023-08-20 15:23:36 +0900

    ルーター設定

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const PORT = 3000
 
 //ルーター呼び出し
 const usersRouter = require('./routes/users')
+const authRouter = require('./routes/auth')
+const postsRouter = require('./routes/posts')
 
 //ミドルウェア
-//ベースURI
+//ベースURIの割り当て
 app.use('/api/users', usersRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/posts', postsRouter)
 
 //エンドポイント
 app.get('/', (req, res) => {
@@ -18,4 +22,4 @@ app.get('/', (req, res) => {
 //   res.send('hello users')
 // })
 
-app.listen(PORT, () => console.log('サーバー起動'))
+app.listen(PORT, () => console.log('サーバー起動:http://localhost:3000/'))
