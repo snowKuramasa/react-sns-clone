8475ba0ed4ec68b8e250b55a8b32523a764a1091
kuramasa <kuramasa.green@gmail.com>
2023-08-22 22:57:03 +0900

    特定の投稿取得api追加

--- routes/posts.js
+++ routes/posts.js
@@ -13,6 +13,19 @@ router.post('/', async (req, res) => {
   }
 })
 
+//特定の投稿を取得する
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    await post.updateOne({
+      $set: req.body,
+    })
+    return res.status(200).json(post)
+  } catch (err) {
+    return res.status(404).json(err)
+  }
+})
+
 //投稿を更新する
 router.put('/:id', async (req, res) => {
   try {
